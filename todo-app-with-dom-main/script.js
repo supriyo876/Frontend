a275c810e73b6ffd829677abb8cd90cf414fe9e3
supriyo876 <supriyochakraborty456@gmail.com>
2026-05-09 23:54:31 +0600

    a dynamic regestration  page

---
+++ todo-app-with-dom-main/script.js
@@ -0,0 +1,10 @@
+console.log(document.head);
+console.log(document.all);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.getElementById('new-task'));
+
+
+
+
+document.title = 'play with dom';
